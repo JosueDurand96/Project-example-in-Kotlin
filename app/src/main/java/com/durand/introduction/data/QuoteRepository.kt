eab26e4a3dbb7f930aba88f6a3282121c44696c8
JosueDurand96 <josuedurandf@gmail.com>
2023-01-13 16:08:41 -0500

    provider

---
+++ app/src/main/java/com/durand/introduction/data/QuoteRepository.kt
@@ -0,0 +1,15 @@
+package com.durand.introduction.data
+
+import com.durand.introduction.data.model.QuoteModel
+import com.durand.introduction.data.model.QuoteProvider
+import com.durand.introduction.data.network.QuoteService
+
+class QuoteRepository {
+    private val api = QuoteService()
+
+    suspend fun getAllQuote():List<QuoteModel>{
+        val response = api.getQuote()
+        QuoteProvider.q
+        return response
+    }
+}
--- app/src/main/java/com/durand/introduction/data/model/QuoteProvider.kt
+++ app/src/main/java/com/durand/introduction/data/model/QuoteProvider.kt
@@ -2,21 +2,6 @@ package com.durand.introduction.data.model
 
 class QuoteProvider {
     companion object {
-
-        fun random(): QuoteModel {
-            val position = (0..6).random()
-            return quote[position]
-        }
-
-        private val quote = listOf<QuoteModel>(
-            QuoteModel(quote = "Example 0", author = "Josue Durand"),
-            QuoteModel(quote = "Example 1", author = "Josh Durand"),
-            QuoteModel(quote = "Example 2", author = "Maria"),
-            QuoteModel(quote = "Example 3", author = "Jesus"),
-            QuoteModel(quote = "Example 4", author = "Haydee"),
-            QuoteModel(quote = "Example 5", author = "Sofia"),
-            QuoteModel(quote = "Example 6", author = "Jennifer")
-        )
+        var quotes: List<QuoteModel> = emptyList()
     }
-
 }
