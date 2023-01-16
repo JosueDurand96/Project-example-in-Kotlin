e717e96139aa7b8079011631320ef34e5318adf0
JosueDurand96 <josuedurandf@gmail.com>
2023-01-16 14:19:06 -0500

    configuration test

--- app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
@@ -1,11 +1,13 @@
 package com.durand.introduction.domain
 
 import com.durand.introduction.data.QuoteRepository
+import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Before
 
 
 class GetQuotesUseCaseTest{
 
+    @RelaxedMockK
     private lateinit var repository: QuoteRepository
 
     @Before
