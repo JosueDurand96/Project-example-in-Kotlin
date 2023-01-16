ffbaafd459649e1fd3280aa6206a02004abd4eca
JosueDurand96 <josuedurandf@gmail.com>
2023-01-16 14:36:15 -0500

    test

--- app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.durand.introduction.domain
 
 import com.durand.introduction.data.QuoteRepository
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -11,19 +12,23 @@ import org.junit.Test
 class GetQuotesUseCaseTest {
 
     @RelaxedMockK
-    private lateinit var repository: QuoteRepository
+    private lateinit var quoteRepository: QuoteRepository
 
     lateinit var getQuotesUseCase: GetQuotesUseCase
 
     @Before
     fun onBefore() {
         MockKAnnotations.init(this)
-        getQuotesUseCase = GetQuotesUseCase(repository)
+        getQuotesUseCase = GetQuotesUseCase(quoteRepository)
     }
 
     @Test
     fun `when the api doesnt return anything then get values from database`() = runBlocking {
+        //Given (Cuando hay una coroutinas se pone el "co")
+        coEvery { quoteRepository.getAllQuotesFromApi() } returns emptyList()
+        //When
 
+        //Then
     }
 
 }
