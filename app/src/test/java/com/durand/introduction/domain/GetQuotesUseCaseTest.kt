28aac0dc9a337e3d32c5b383a788e91cfcee663f
JosueDurand96 <josuedurandf@gmail.com>
2023-01-16 14:25:37 -0500

    test

--- app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
@@ -1,17 +1,28 @@
 package com.durand.introduction.domain
 
 import com.durand.introduction.data.QuoteRepository
+import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 
-class GetQuotesUseCaseTest{
+class GetQuotesUseCaseTest {
 
     @RelaxedMockK
     private lateinit var repository: QuoteRepository
 
+    lateinit var getQuotesUseCase: GetQuotesUseCase
+
     @Before
-    fun onBefore(){
+    fun onBefore() {
+        MockKAnnotations.init(this)
+        getQuotesUseCase = GetQuotesUseCase(repository)
+    }
+
+    @Test
+    fun `when the api doesnt return anything then get values from database`() = runBlocking {
 
     }
 
