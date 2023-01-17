8793294602cd9ce942832307ebd3fda482029625
JosueDurand96 <josuedurandf@gmail.com>
2023-01-16 19:09:23 -0500

    test

--- app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/durand/introduction/domain/GetQuotesUseCaseTest.kt
@@ -1,6 +1,7 @@
 package com.durand.introduction.domain
 
 import com.durand.introduction.data.QuoteRepository
+import com.durand.introduction.domain.model.Quote
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -8,7 +9,6 @@ import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
-
 
 class GetQuotesUseCaseTest {
 
@@ -27,10 +27,29 @@ class GetQuotesUseCaseTest {
     fun `when the api doesnt return anything then get values from database`() = runBlocking {
         //Given (Cuando hay una coroutinas se pone el "co")
         coEvery { quoteRepository.getAllQuotesFromApi() } returns emptyList()
+
         //When
         getQuotesUseCase()
+
         //Then
+        coVerify(exactly = 0) { quoteRepository.clearQuotes() }
+        coVerify(exactly = 0) { quoteRepository.insertQuotesDatabase(any()) }
         coVerify(exactly = 1) { quoteRepository.getAllQuotesFromDatabase() }
+    }
+
+    @Test
+    fun `when the api doesnt return anything then get values from api`() = runBlocking {
+        //Given (Cuando hay una coroutinas se pone el "co")
+        val myList = listOf(Quote("Hola Mundo","Josue Durand"))
+        coEvery { quoteRepository.getAllQuotesFromApi() } returns myList
+
+        //When
+        val response = getQuotesUseCase()
+
+        //Then
+        coVerify(exactly = 1) { quoteRepository.clearQuotes() }
+        coVerify(exactly = 1) { quoteRepository.insertQuotesDatabase(any()) }
+        coVerify(exactly = 0) { quoteRepository.getAllQuotesFromDatabase() }
     }
 
 }
